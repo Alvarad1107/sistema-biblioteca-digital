fc83f22ad794de9813fcd02c6a069b92ff1bb606
BlackSpot-12 <jeffersonmenfoza811@gmail.com>
2026-06-16 20:13:59 -0600

    corrección de errores

--- libros/libro_guardar.php
+++ libros/libro_guardar.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['usuario_id'])) {
-    header("Location: libro_lista.php?status=guardado");
+if (!isset($_SESSION['correo'])) {
+    header("Location: ../index.php");
     exit();
 }
 
@@ -12,17 +12,20 @@ $codigo = $_POST['codigo'];
 $titulo = $_POST['titulo'];
 $autor = $_POST['autor'];
 $editorial = $_POST['editorial'];
-$anio_publicacion = empty($_POST['anio_publicacion']) ? 'NULL' : $_POST['anio_publicacion'];
 $categoria_id = $_POST['categoria_id'];
 $stock = $_POST['stock'];
 
+$anio_publicacion = empty($_POST['anio_publicacion']) ? "NULL" : "'" . $_POST['anio_publicacion'] . "'";
+
 $sql = "INSERT INTO libros (codigo, titulo, autor, editorial, anio_publicacion, categoria_id, stock) 
-        VALUES ('$codigo', '$titulo', '$autor', '$editorial', $anio_publicacion, $categoria_id, $stock)";
+        VALUES ('$codigo', '$titulo', '$autor', '$editorial', $anio_publicacion, '$categoria_id', '$stock')";
 
 if ($conn->query($sql) === TRUE) {
-    header("Location: libro_form.php?status=success");
+    
+    header("Location: libro_lista.php");
     exit();
 } else {
+   
     header("Location: libro_form.php?status=error");
     exit();
 }
